b6ccb3a784bfd4c47a9ebc9fa9d88004645ba679
matheus <matheus-depaula@hotmail.com>
2020-10-10 23:36:14 -0300

    master

--- main.js
+++ main.js
@@ -1,43 +1,59 @@
-// Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
+'use strict';
 
-function createWindow () {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+var app = require('electron').app;
+var BrowserWindow = require('electron').BrowserWindow;
+var mainWindow = null;
+var ipc = require('electron').ipcMain;
 
-  // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+ipc.on('close-main-window', function() {
+    app.quit();
+});
 
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-}
+app.on('ready', function() {
+    mainWindow = new BrowserWindow({
+        resizable: true,
+        height: 600,
+        width: 800,
+        webPreferences:{
+          nodeIntegration:true
+        }
+    });
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  createWindow()
-  
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+    mainWindow.loadFile('index.html')
+    mainWindow.on('closed', function() {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
+    });
+});
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
+var os = require('os');
+var {dialog} = require('electron');
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+ipc.on('open-file-dialog-for-file', function (event) {
+    if(os.platform() === 'linux' || os.platform() === 'win32'){
+      //  dialog.showOpenDialog({
+      //      properties: ['openFile']
+      //  }, function (files) {
+      //     if (files) event.sender.send('selected-file', files[0]);
+      //  });
+      dialog.showOpenDialog({
+        properties: ['openFile']
+     }).then((data) => {
+        // console.log(data.filePaths);
+        event.sender.send('selected-file', data.filePaths);
+     });
+   } else {
+      //  }, function (files) {
+      //      if (files) event.sender.send('selected-file', files[0]);
+      //  });
+      dialog.showOpenDialog({
+        properties: ['openFile','openDirectory']
+     }).then((data) => {
+        // console.log(data.filePaths);
+        event.sender.send('selected-file', data.filePaths);
+     });
+   }});
+
+   
--- renderer.js
+++ renderer.js
@@ -4,3 +4,22 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+
+var loki = require('lokijs');
+var db = new loki('db.json');
+var clientes = db.addCollection('clientes')
+clientes.insert({
+    nome: 'leonardo',
+    email:'teste@gmail.com'
+});
+db.save()
+
+const ipc = require('electron').ipcRenderer;
+const buttonCreated = document.getElementById('upload');
+
+buttonCreated.addEventListener('click', function (event) {
+    ipc.send('open-file-dialog-for-file')
+});
+ipc.on('selected-file', function (event, path) {
+    console.log('Full path: ', path);
+});
